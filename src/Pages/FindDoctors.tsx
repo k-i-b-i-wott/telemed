52fbadb454bc96bfabd96be6109014f5ed169bf0
Kibiwott <bk.kibiwott@gmail.com>
2025-04-30 09:25:01 +0300

    initialized the doctors page

--- src/Pages/FindDoctors.tsx
+++ src/Pages/FindDoctors.tsx
@@ -1,4 +1,4 @@
-import {Box, Button, FormControl, FormControlLabel, InputLabel, MenuItem, Select, TextField, Typography} from "@mui/material"
+import {Box, Button, Card, CardActions,Avatar, CardContent, FormControl,  MenuItem, Select, TextField, Typography} from "@mui/material"
 
 const FindDoctors = () => {
   return (
@@ -31,7 +31,8 @@ const FindDoctors = () => {
             boxShadow:2,
             display:"flex",
             justifyContent:"space-around",
-            mt:3
+            mt:3,
+            alignItems:"center"
             
         }}>
             <Box sx={{
@@ -84,19 +85,110 @@ const FindDoctors = () => {
             </Box>             
             <Box sx={{
                   flex:1,
-                  gap:1,
-                  p:3,
+                  gap:1,                  
                   alignItems:"center",
                   justifyContent:"center"
             }}>
-            <Button variant="contained" sx={{
-                backgroundColor:"darkseagreen",
-                                                
-            }}>
-                    Apply Filters                
-            </Button> 
+                <Button variant="contained"
+                sx={{
+                    backgroundColor:"darkseagreen",
+                    width:300
+                }}
+                >
+                    Apply Filters                                        
+                </Button>                     
             </Box> 
-        </Box>   
+        </Box>
+        <Box sx={{
+            display:"flex",
+            alignItems:"center",
+            justifyContent:"center",
+            flexWrap:"wrap",
+            boxShadow:1,
+            borderBlockColor:"darkseagreen",
+            p:4
+        }}>
+            <Card>
+                <CardContent>
+                <Box sx={{
+                    display:"flex",
+                    flexDirection:"row",
+                    
+                    alignItems:"center",
+                    padding:"1rem"
+                }}>
+                    <Avatar sx={{width:100, height:100}}>
+
+                    </Avatar>
+                    <Box sx={{
+                        display:"flex",
+                        flexDirection:"column",
+                        marginLeft:3
+                    }}>
+                        <Typography sx={{
+                            fontWeight:600,
+                            color:"darkseagreen"
+
+                        }}>
+                            Dr. John Doe
+                        </Typography>
+                        <Typography sx={{
+                            fontWeight:100,
+                            fontStyle:"italic",
+                        }}>
+                            Cardiologist
+                        </Typography>
+                    </Box>                    
+                </Box>  
+                <Box sx={{
+                    display:"flex",
+                    flexDirection:"row",
+                    justifyContent:"space-between",
+                    padding:3
+                }}>
+                    <Box sx={{
+                        display:"flex",
+                        flexDirection:"row",
+                        gap:2
+                    }}>
+                        <Typography sx={{
+                            fontWeight:600,
+                            fontSize:15
+                        }}>
+                            Experience :  
+                        </Typography>
+                        <Typography sx={{
+                            
+                        }}>
+                            15 Years
+                        </Typography>
+                    </Box>
+                    <Typography>
+                        <span>⭐</span> 4.5/5
+                    </Typography>
+                </Box>
+                            
+                </CardContent>   
+                <CardActions>
+                <Box sx={{
+                    display:"flex",
+                    flexDirection:"row",
+                    justifyContent:"space-between",
+                    gap:2
+                    
+                }}>
+                    <Button variant='outlined' sx={{
+                        color:"darkgreen"
+                    }}>View Profile</Button>
+                    <Button variant='contained' sx={{
+                        bgcolor:"darkgreen"
+                    }}>Book Appointment</Button>
+                    
+                </Box>   
+                </CardActions>             
+            </Card>
+        </Box>  
+
     </Box>
   )
 }
