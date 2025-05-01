6a2a63f72d69884594c93015ccd3741f63b78cd6
Kibiwott <bk.kibiwott@gmail.com>
2025-05-01 23:19:06 +0300

    Added the about section to the profile

--- src/Pages/DoctorsProfile.tsx
+++ src/Pages/DoctorsProfile.tsx
@@ -1,7 +1,14 @@
 
-import{Avatar, Box, Button, Divider, Typography} from '@mui/material'
-
+import{Avatar, Box, Button, Divider, Tab,  Typography} from '@mui/material'
+import { useState } from 'react';
+import {TabContext, TabPanel,TabList} from "@mui/lab"
 const DoctorsProfile = () => {
+  const [value, setValue] = useState('1');
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    event.preventDefault()
+    setValue(newValue);
+  };
   return (
     <Box sx={{
       mt:17,
@@ -9,7 +16,10 @@ const DoctorsProfile = () => {
       flexDirection:{
         xs:"column",
         md:"row"
-      }
+      },
+      gap:3,  
+      width:"100vw"      
+      
     }}>
       <Box boxShadow={2} sx={{
         display:"flex",
@@ -28,8 +38,8 @@ const DoctorsProfile = () => {
       <Avatar sx={{width:100, height:100}}></Avatar>   
       <Typography variant='h5'>Dr. John Doe</Typography>
       <Typography variant='body1'><span>⭐</span> 4.5/5(3 reviews)</Typography>
-      <Button variant='contained' fullWidth sx={{mb:2}}>View Profile</Button> 
-      <Button variant='outlined' fullWidth sx={{mb:2}}>Send Message</Button>
+      <Button variant='contained' fullWidth sx={{mb:2, backgroundColor:"seagreen"}}>View Profile</Button> 
+      <Button variant='outlined' fullWidth sx={{mb:2,color:"seagreen"}}>Send Message</Button>
       </Box>
       <Divider />
       <Box sx={{
@@ -97,9 +107,29 @@ const DoctorsProfile = () => {
       </Box>
       </Box> 
       <Box>
-        <Box>
-                  
-        </Box>        
+      <Box sx={{  typography: 'body1',
+        flexGrow: 1,
+        width: "100%", 
+        display: "flex",
+        flexDirection: "column",
+        mt: 3,
+       }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, display: "flex", flexDirection: "row", width: "100%", alignItems: "center",justifyContent:"space-around", borderColor: 'divider' }}>
+          <TabList onChange={handleChange}  sx={{width:"100%",
+          display:"flex",
+          justifyContent:"space-around"
+          }}>
+            <Tab label="About" value="1" />
+            <Tab label="Availability" value="2" />
+            <Tab label="Reviews" value="3" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">About</TabPanel>
+        <TabPanel value="2">Availability</TabPanel>
+        <TabPanel value="3">Reviews</TabPanel>
+      </TabContext>
+    </Box>      
       </Box>                       
     </Box>
   )
