ff6f943041ca3fd6386018da52cfcc5c0dee230c
Kibiwott <bk.kibiwott@gmail.com>
2025-05-12 08:46:14 +0300

    Added the login functionality

--- src/Pages/SignIn.tsx
+++ src/Pages/SignIn.tsx
@@ -1,12 +1,48 @@
-import { Box ,Grid,Typography,Checkbox, Paper, TextField,Avatar,Button,FormControlLabel} from '@mui/material'
+import { Box ,Grid,Typography,Checkbox, Paper, TextField,Avatar,Button,FormControlLabel, Alert} from '@mui/material'
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { useMutation } from '@tanstack/react-query'
+import apiUrl from '../utils/apiUrl'
 
 const SignIn = () => {
-    const [emailAddress, setEmailAddress]= useState("")
+    const [identifier, setIdentifier]= useState("")
     const [password, SetPassword] =useState("")
-    const hadleSubmit = () =>{
-        
+    const [error, setError] = useState("")
+    const navigate = useNavigate()
+
+    const {mutate,isPending}=useMutation({
+        mutationKey:["Login-User"],
+        mutationFn: async () =>{
+            const response = await axios.post(`${apiUrl}/auth/login`,{
+                identifier,
+                password
+            },{withCredentials:true})
+            console.log(response.data)    
+            return response.data
+        },
+        onSuccess:()=>{
+            // navigate('/')
+        },
+        onError:(error)=>{
+            if(axios.isAxiosError(error)){
+                setError(error.response?.data.message)
+            }else{
+                setError("An error occurred")
+            }
+        }
+    })
+
+
+
+    const handleSubmit = (e:React.FormEvent<HTMLFormElement>) =>{
+        e.preventDefault()
+        setError("")        
+        if (!identifier || !password){
+            setError("All fields required")
+            return
+        }
+        mutate ()        ;
     }
   return (
     <Box sx={{
@@ -34,12 +70,18 @@ const SignIn = () => {
             </Box>   
             <Box
             component={"form"}
+            onSubmit={handleSubmit}
             sx={{
                 maxWidth:500
             }}
-            >              
-                <TextField label="Enter your Email or Username" type='email' variant="outlined" 
+            >     
+                {
+                error && <Alert severity='error' sx={{mb:2}}>{error}</Alert> 
+                }         
+                <TextField label="Enter your Email or Username"  variant="outlined" 
                  fullWidth
+                 value={identifier}
+                 onChange={(e)=> setIdentifier(e.target.value)}
                  sx={{
                     mb:2
                  }}
@@ -47,6 +89,8 @@ const SignIn = () => {
                 
                 <TextField label="Password" type='password' variant="outlined" 
                  fullWidth
+                 value={password}
+                 onChange={(e)=>SetPassword(e.target.value)}
                  sx={{
                     mb:2
                  }}
@@ -59,9 +103,11 @@ const SignIn = () => {
                     mv:2
                  }}
                     />
-                <Button variant="contained" type='submit' size="large" fullWidth  sx={{
+                <Button variant="contained" type='submit' size="large" fullWidth disabled={isPending}  sx={{
                     mb:2
-                 }}>Sign In</Button>
+                 }}>{
+                    isPending ? "Logging In ....." : "Login"
+                 }</Button>
             </Box>  
 
             <Grid container sx={{gap:1}}>
