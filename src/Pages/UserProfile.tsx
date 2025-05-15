824069191ff7366063ca9c2c3cf25d56062b0c1e
Kibiwott <bk.kibiwott@gmail.com>
2025-05-15 22:32:55 +0300

    Customized the profile

--- src/Pages/UserProfile.tsx
+++ src/Pages/UserProfile.tsx
@@ -1,14 +1,87 @@
-import { Box,Typography,Paper} from '@mui/material'
+import { Box,Typography,Avatar,Button} from '@mui/material'
 
 const UserProfile = () => {
   return (
     <Box  sx={{
-      mt:17
+      mt:17,
+      display:'flex',
+      flexDirection:{
+        sx:"column",
+        md:"row"
+      }
     }}>
-      <Box>
-              
+      <Box sx={{
+        display:"flex",
+        flexDirection:"Column",
+        justifyContent:"space-between",
+        margin:2,
+        p:3,
+        alignItems:"center",
+        boxShadow:3,
+        width:200
+      }}>   
+        <Avatar>
+        </Avatar>
+        <Box sx={{
+          display:"flex",
+          flexDirection:"column",
+          gap:1,
+          fontSize:10,
+        }}>
+          <Typography variant='h5'>
+            Name          
+          </Typography>  
+          <Typography variant='body1'>
+            Email
+          </Typography>
+        </Box>
+        <Typography>
+            Member since date
+        </Typography>
+        <Box sx={{
+          display:"flex",
+          flexDirection:"column"                    ,
+          gap:2,
+          aligItems:"left"
+        }}>
+          <Button 
+          fullwidth
+          sx={{
+            variant:"outlined",
+            fontSize:12            
+          }}>
+            Personal Information
+          </Button>
+          <Button
+           fullwidth
+           sx={{
+             variant:"outlined",
+             fontSize:12            
+           }}
+          >
+            Appointments
+          </Button>
+          <Button
+           fullwidth
+           sx={{
+             variant:"outlined",
+             fontSize:12            
+           }}
+          >
+            Medical Records
+          </Button> 
+            <Button
+             fullwidth
+             sx={{
+               variant:"outlined",
+               fontSize:12            
+             }}            
+            >
+              Settings
+            </Button>         
+        </Box>
       </Box>        
-    </Box>
+    </Box>  
   )
 }
 
